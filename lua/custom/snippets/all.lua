e81eed364c25b2866cfdc1b9b70deabb39c69d03
anvnh <73346280+anvnh@users.noreply.github.com>
2026-03-16 01:45:36 +0700

    feat(snippets): add section comment snippet
    Adds a new 'sec' snippet for generating section comments.
    Refactors snippet file structure by renaming 'cp/cpp.lua' to 'cpp.lua'.
    Applies minor style fixes, including consistent string quotes and formatting.
    Updates C++ compilation to suppress unused variable warnings.

--- lua/custom/snippets/all.lua
+++ lua/custom/snippets/all.lua
@@ -1,5 +1,30 @@
-local ls = require 'luasnip'
+local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
 
-return {}
+local function comment_prefix()
+      local cs = vim.bo.commentstring
+      if cs == "" then
+            return "//"
+      end
+      return cs:match("^(.*)%%s"):gsub("%s*$", "")
+end
+
+ls.add_snippets("all", {
+      s("sec", {
+            f(function()
+                  return comment_prefix() .. " --------------------------------"
+            end, {}),
+            t({ "", "" }),
+            f(function()
+                  return comment_prefix() .. " "
+            end, {}),
+            i(1, "Section"),
+            t({ "", "" }),
+            f(function()
+                  return comment_prefix() .. " --------------------------------"
+            end, {}),
+      }),
+})
