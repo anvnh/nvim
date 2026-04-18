fad697a0239a71c3a717e4279d7e6deb595be0ff
anvnh <73346280+anvnh@users.noreply.github.com>
2026-04-18 23:18:55 +0700

    refactor(plugins): update configurations and dependencies
    Refactor treesitter to use autocmd for language-specific setup.
    Simplify leap.nvim configuration and update its source URL.
    Update plugin hashes in lazy-lock.json.
    Add empty .codex file.

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,9 +1,7 @@
 return {
-  'ggandor/leap.nvim',
-  config = function()
-    require('leap').setup {
-      vim.keymap.set({ 'n', 'x', 'o' }, 'zj', '<Plug>(leap-forward)'),
-      vim.keymap.set({ 'n', 'x', 'o' }, 'zk', '<Plug>(leap-backward)'),
-    }
-  end,
+    url = "https://codeberg.org/andyg/leap.nvim",
+    config = function()
+        vim.keymap.set({ "n", "x", "o" }, "zj", "<Plug>(leap-forward)", { desc = "Leap forward" })
+        vim.keymap.set({ "n", "x", "o" }, "zk", "<Plug>(leap-backward)", { desc = "Leap backward" })
+    end,
 }
